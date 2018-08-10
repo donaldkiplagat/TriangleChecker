50895802888da4d060e97800af27d71490f45a20
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-10 13:06:08 +0300

    Condition for checking if there's a value entered

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ var a= parseFloat(prompt("Enter side A: "));
   if (isNaN(a)=== true){
     alert("Please enter a number in the field");
 }else{
+  var b= parseFloat(prompt("Enter side B: "));
+    if (isNaN(b)=== true){
+        alert("Please enter a number in the field");
+      }else{
+        var c= parseFloat(prompt("Enter side C: "));
+          if (isNaN(c)=== true){
+            alert("Please enter a number in the field");
+          }else{
+            if((a+b)<=c || (b+c)<=a || (a+c)<=b){
+              alert("Not a Triangle");
+            }
