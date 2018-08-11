4715b4132c444e295e7bacb1b71a440aae2330b4
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-11 15:08:53 +0300

    Proper spacing and indentation done on Javascript file

--- js/script.js
+++ js/script.js
@@ -4,33 +4,33 @@ var checker=[];
 checker.push(parseFloat(prompt("Enter side A: ")));
   if (isNaN(checker[0])=== true){
     alert("Please enter a number in the field");
-}else{
+  }else{
   checker.push(parseFloat(prompt("Enter side B: ")));
     if (isNaN(checker[1])=== true){
         alert("Please enter a number in the field");
       }else{
-        checker.push(parseFloat(prompt("Enter side C: ")));
-          if (isNaN(checker[2])=== true){
+      checker.push(parseFloat(prompt("Enter side C: ")));
+        if (isNaN(checker[2])=== true){
             alert("Please enter a number in the field");
-          }else{
-            if((checker[0]+checker[1])<=checker[2] || (checker[1]+checker[2])<=checker[0] || (checker[0]+checker[2])<=checker[1]){
-              alert("Not a Triangle");
-            }
-            else if(checker[0]===checker[1] && checker[0]===checker[2] && checker[1]===checker[2]){
-              alert("Equilateral");
-            }
-            else if(checker[0]===checker[1] || checker[1]===checker[2] || checker[0]===checker[2]){
-              alert("Isosceles");
-            }
-            else if((checker[0]+checker[1])>=checker[2] || (checker[1]+checker[2])>=checker[0] || (checker[0]+checker[2])>=checker[1]){
-              alert("Scalene");
-            }
-            else{
-              alert("Value entered must be a number");
-            }
-          }
+        }else{
+        if((checker[0]+checker[1])<=checker[2] || (checker[1]+checker[2])<=checker[0] || (checker[0]+checker[2])<=checker[1]){
+            alert("Not a Triangle");
+        }
+        else if(checker[0]===checker[1] && checker[0]===checker[2] && checker[1]===checker[2]){
+            alert("Equilateral");
+        }
+        else if(checker[0]===checker[1] || checker[1]===checker[2] || checker[0]===checker[2]){
+            alert("Isosceles");
+        }
+        else if((checker[0]+checker[1])>=checker[2] || (checker[1]+checker[2])>=checker[0] || (checker[0]+checker[2])>=checker[1]){
+            alert("Scalene");
+        }
+        else{
+            alert("Value entered must be a number");
         }
       }
+    }
+  }
 }
 
 
