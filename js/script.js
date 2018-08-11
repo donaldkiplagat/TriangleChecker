4d2fa13e38266f8c3d019f7b47c86a3c1da832eb
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-08-11 14:15:10 +0300

    Array used to replace variables

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 var answer=function(){
+var checker=[];
 
-var a= parseFloat(prompt("Enter side A: "));
-  if (isNaN(a)=== true){
+checker.push(parseFloat(prompt("Enter side A: ")));
+  if (isNaN(checker[0])=== true){
     alert("Please enter a number in the field");
 }else{
-  var b= parseFloat(prompt("Enter side B: "));
-    if (isNaN(b)=== true){
+  checker.push(parseFloat(prompt("Enter side B: ")));
+    if (isNaN(checker[1])=== true){
         alert("Please enter a number in the field");
       }else{
-        var c= parseFloat(prompt("Enter side C: "));
-          if (isNaN(c)=== true){
+        checker.push(parseFloat(prompt("Enter side C: ")));
+          if (isNaN(checker[2])=== true){
             alert("Please enter a number in the field");
           }else{
-            if((a+b)<=c || (b+c)<=a || (a+c)<=b){
+            if((checker[0]+checker[1])<=checker[2] || (checker[1]+checker[2])<=checker[0] || (checker[0]+checker[2])<=checker[1]){
               alert("Not a Triangle");
             }
-            else if(a===b && a===c && b===c){
+            else if(checker[0]===checker[1] && checker[0]===checker[2] && checker[1]===checker[2]){
               alert("Equilateral");
             }
-            else if(a===b || b===c || a===c){
+            else if(checker[0]===checker[1] || checker[1]===checker[2] || checker[0]===checker[2]){
               alert("Isosceles");
             }
-            else if((a+b)>=c || (b+c)>=a || (a+c)>=b){
+            else if((checker[0]+checker[1])>=checker[2] || (checker[1]+checker[2])>=checker[0] || (checker[0]+checker[2])>=checker[1]){
               alert("Scalene");
             }
             else{
@@ -31,3 +32,38 @@ var a= parseFloat(prompt("Enter side A: "));
         }
       }
 }
+
+
+// var answer=function(){
+//
+// var a= parseFloat(prompt("Enter side A: "));
+//   if (isNaN(a)=== true){
+//     alert("Please enter a number in the field");
+// }else{
+//   var b= parseFloat(prompt("Enter side B: "));
+//     if (isNaN(b)=== true){
+//         alert("Please enter a number in the field");
+//       }else{
+//         var c= parseFloat(prompt("Enter side C: "));
+//           if (isNaN(c)=== true){
+//             alert("Please enter a number in the field");
+//           }else{
+//             if((a+b)<=c || (b+c)<=a || (a+c)<=b){
+//               alert("Not a Triangle");
+//             }
+//             else if(a===b && a===c && b===c){
+//               alert("Equilateral");
+//             }
+//             else if(a===b || b===c || a===c){
+//               alert("Isosceles");
+//             }
+//             else if((a+b)>=c || (b+c)>=a || (a+c)>=b){
+//               alert("Scalene");
+//             }
+//             else{
+//               alert("Value entered must be a number");
+//             }
+//           }
+//         }
+//       }
+// }
